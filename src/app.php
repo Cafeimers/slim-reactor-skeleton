e219e6baaa45835f2ee72623137ce0e9a9289d7a
Piotr Hałas <Piotr.Halas@komputronik.pl>
2018-02-06 15:39:55 +0100

    fix example to use SlimReactorApp

--- src/app.php
+++ src/app.php
@@ -1,7 +1,7 @@
 #!/usr/bin/env php
 <?php
 
-use CrazyGoat\SlimReactor\SlimReactor;
+use CrazyGoat\SlimReactor\{SlimReactor, SlimReactorApp};
 
 if (PHP_SAPI != 'cli') {
     throw new RuntimeException('Can only run in cli');
@@ -11,7 +11,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 // Instantiate the app
 $settings = require __DIR__ . '/../src/settings.php';
-$app = new \Slim\App($settings);
+$app = new SlimReactorApp($settings);
 
 // Set up dependencies
 require __DIR__ . '/../src/dependencies.php';
@@ -22,5 +22,10 @@ require __DIR__ . '/../src/middleware.php';
 // Register routes
 require __DIR__ . '/../src/routes.php';
 
-$slimReactor  = new SlimReactor($app, isset($argv[1]) ? $argv[1] : '0.0.0.0:0');
+$slimReactor  = new SlimReactor(
+    $app,
+    [
+        'socket' => isset($argv[1]) ? $argv[1] : '0.0.0.0:0'
+    ]
+);
 $slimReactor->run();
